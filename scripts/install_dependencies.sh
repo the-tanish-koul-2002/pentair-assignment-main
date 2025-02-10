c466b7fafa5a38234ceeb36ed2a2821fe4259062
the-tanish-koul-2002 <thetanishkoul@gmail.com>
2025-02-11 01:27:50 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install -y docker
+sudo yum update -y
+sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
-docker pull 216989092759.dkr.ecr.us-east-1.amazonaws.com/my-weather-app:latest
+docker pull <AWS_ACCOUNT_ID>.dkr.ecr.us-east-1.amazonaws.com/my-weather-app:latest
