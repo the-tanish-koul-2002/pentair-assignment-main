bf979d82a59862d3a74ec1ae44438e9576e0fd5a
the-tanish-koul-2002 <thetanishkoul@gmail.com>
2025-02-11 02:09:06 +0530

    Ho jayega bhai

--- scripts/install_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo yum install -y docker
-sudo systemctl start docker
-sudo systemctl enable docker
-docker pull 216989092759.dkr.ecr.us-east-1.amazonaws.com/my-weather-app:latest
